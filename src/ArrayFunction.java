c8918769115f6376db4f476c37a46ae9566782c0
Shihnaz <60668360+Shihnaz@users.noreply.github.com>
2020-02-08 20:32:26 +0200

    Update ArrayFunction.java

--- src/ArrayFunction.java
+++ src/ArrayFunction.java
@@ -1,12 +1,12 @@
-import javax.swing.*;// this is example of string concatination and array and introduction to gui
+
 public class ArrayFunction {
 
 	public static void main(String[] args) { 
 	int a[]= {5,6,7,8,9,10,11,12,17,19,20};
-	String sh="j\tA[J]\n"; int j=0;
+	String sh="shihnaz"; int j=0;
 	
 	for(int f:a) {sh+=j+"\t"+f+"\n";j++;}
-	JTextArea w=new JTextArea(sh);
-	JOptionPane.showMessageDialog(null, w, "this is my new work", JOptionPane.INFORMATION_MESSAGE);
+	
+	
 	
 	}}
